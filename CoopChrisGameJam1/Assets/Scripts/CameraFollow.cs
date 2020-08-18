f84ed5378c016c059b189014c0c46a1fadd5021e
CooperRapp <61958386+CooperRapp@users.noreply.github.com>
2020-08-18 00:19:07 -0700

    ChangesMore
    camera script + unity stuff

---
+++ CoopChrisGameJam1/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform player;
+
+    public Vector3 offset = new Vector3(0f, 0f, 0f);
+
+    void LateUpdate()
+    {
+
+        transform.position = player.position + offset;
+
+    }
+}
