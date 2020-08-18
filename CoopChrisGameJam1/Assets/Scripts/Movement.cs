f6c8d8b81fb73f377025872d3ff3238aa3493e00
CooperRapp <61958386+CooperRapp@users.noreply.github.com>
2020-08-17 22:33:53 -0700

    Added Tiles

---
+++ CoopChrisGameJam1/Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+
+    public float speed = 5.0f;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+
+    }
+
+    void FixedUpdate()
+    {
+
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+
+    }
+
+}
