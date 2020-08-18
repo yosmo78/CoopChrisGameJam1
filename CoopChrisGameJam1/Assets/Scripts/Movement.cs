59e7439658c318a3008e94cc1aedd12f53498d76
yosmo78 <yosmo78@gmail.com>
2020-08-18 00:19:41 -0700

    scripty

--- CoopChrisGameJam1/Assets/Scripts/Movement.cs
+++ CoopChrisGameJam1/Assets/Scripts/Movement.cs
@@ -8,14 +8,16 @@ public class Movement : MonoBehaviour
 
     public Rigidbody2D rb;
 
-    public float accelerationFactor = 10f;
-    public float decelerationFactor = 1f;
+    public float accelerationFactor = 100.0f;
+    public float decelerationFactor = 1.0f;
+
+    public float maxVelocity = 4.0f;
 
     Vector2 acceleration;
-    Vector2 velocity;
+    //Vector2 velocity;
 
 
- public float timeFromZeroToMax = 2;  // How long it takes to reach max
+ public float timeFromZeroToMax = 2.0f;  // How long it takes to reach max
  
  void Update ()
  {
@@ -26,39 +28,65 @@ public class Movement : MonoBehaviour
      if (Input.GetAxisRaw("Horizontal") == 1)
      {
 
-         moveTowardsX = 1.0f;
-         transform.localRotation = Quaternion.Euler(0, 0, 0);
+         moveTowardsX = maxVelocity;
 
         }
      else if (Input.GetAxisRaw("Horizontal") == -1)
      {
 
-         moveTowardsX = -1.0f;
-         transform.localRotation = Quaternion.Euler(0, 180, 0);
+         moveTowardsX = -maxVelocity;
 
      }
 
      if (Input.GetAxisRaw("Vertical") == 1)
      {
 
-         moveTowardsY = 1.0f;
-         transform.localRotation = Quaternion.Euler(0, 0, 90);
+         moveTowardsY = maxVelocity;
 
      }
      else if (Input.GetAxisRaw("Vertical") == -1)
      {
 
-         moveTowardsY = -1.0f;
-         transform.localRotation = Quaternion.Euler(0, 0, -90);
+         moveTowardsY = -maxVelocity;
 
      }
 
-     float changeRatePerSecondX = (moveTowardsX == 0?decelerationFactor:accelerationFactor) / timeFromZeroToMax * Time.deltaTime;
-     float changeRatePerSecondY = (moveTowardsY == 0?decelerationFactor:accelerationFactor) / timeFromZeroToMax * Time.deltaTime;
+     float changeRatePerSecondX = ((moveTowardsX == 0?decelerationFactor:accelerationFactor) / timeFromZeroToMax) * Time.deltaTime;
+     float changeRatePerSecondY = ((moveTowardsY == 0?decelerationFactor:accelerationFactor) / timeFromZeroToMax) * Time.deltaTime;
 
 
-     velocity.x = Mathf.MoveTowards (velocity.x, moveTowardsX, changeRatePerSecondX);
-     velocity.y = Mathf.MoveTowards (velocity.y, moveTowardsY, changeRatePerSecondY);
+     float vx = Mathf.MoveTowards (rb.velocity.x, moveTowardsX, changeRatePerSecondX);
+
+     if(vx > maxVelocity)
+     {
+        vx = maxVelocity;
+     }
+     else if (vx < -maxVelocity)
+     {
+        vx = -maxVelocity;
+     }
+
+     float vy = Mathf.MoveTowards (rb.velocity.y, moveTowardsY, changeRatePerSecondY);
+
+     if(vy > maxVelocity)
+     {
+        vy = maxVelocity;
+     }
+     else if (vy < -maxVelocity)
+     {
+        vy = -maxVelocity;
+     }
+
+     rb.velocity = new Vector3(vx,vy,0);
+
+     Vector2 moveDirection = rb.velocity;
+     if (moveDirection != Vector2.zero) 
+     {
+         float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+     }
+
+
 
  }
 
@@ -66,13 +94,6 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
-
-        //velocity += (accelerationFactor* acceleration.normalized* Time.deltaTime) + (-1.0f*decelerationFactor* velocity.normalized);
-        Debug.Log(velocity.x);
-
-        rb.position += velocity;
-
-        rb.MovePosition(rb.position);
 
     }
 
