77bf9db3d3495cb1eacdfee0ac3fc6c667423447
yosmo78 <yosmo78@gmail.com>
2020-08-17 23:23:52 -0700

    watch

--- CoopChrisGameJam1/Assets/Scripts/Movement.cs
+++ CoopChrisGameJam1/Assets/Scripts/Movement.cs
@@ -1,30 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System; 
 
 public class Movement : MonoBehaviour
 {
 
     public Rigidbody2D rb;
 
-    public float speed = 5.0f;
+    public float accelerationFactor = 0.005f;
+    public float decelerationFactor = 0.005f;
 
-    Vector2 movement;
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+    Vector2 acceleration;
+    Vector2 velocity;
 
 
-    }
+
+ public float timeFromZeroToMax = 2;  // How long it takes to reach max
+ 
+ void Update ()
+ {
+     float moveTowardsX = 0;
+     float moveTowardsY = 0;
+     if (Input.GetAxisRaw("Horizontal") == 1)
+     {
+         moveTowardsX = 1.0f;
+     }
+     else if (Input.GetAxisRaw("Horizontal") == -1)
+     {
+         moveTowardsX = -1.0f;
+     }
+
+     if (Input.GetAxisRaw("Vertical") == 1)
+     {
+         moveTowardsY = 1.0f;
+     }
+     else if (Input.GetAxisRaw("Vertical") == -1)
+     {
+         moveTowardsY = -1.0f;
+     }
+
+     float changeRatePerSecondX = (moveTowardsX == 0?decelerationFactor:accelerationFactor) / timeFromZeroToMax * Time.deltaTime;
+     float changeRatePerSecondY = (moveTowardsY == 0?decelerationFactor:accelerationFactor) / timeFromZeroToMax * Time.deltaTime;
+
+
+     velocity.x = Mathf.MoveTowards (velocity.x, moveTowardsX, changeRatePerSecondX);
+     velocity.y = Mathf.MoveTowards (velocity.y, moveTowardsY, changeRatePerSecondY);
+ }
+
+
 
     void FixedUpdate()
     {
 
-        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+        //velocity += (accelerationFactor* acceleration.normalized* Time.deltaTime) + (-1.0f*decelerationFactor* velocity.normalized);
+        Debug.Log(velocity.x);
+
+        rb.position += velocity;
+
+        rb.MovePosition(rb.position);
 
     }
 
