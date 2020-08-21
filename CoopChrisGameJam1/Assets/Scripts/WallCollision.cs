c07afaf12c08235bd41eb4b85f31c1876d3a411b
yosmo78 <yosmo78@gmail.com>
2020-08-20 20:26:34 -0700

    only th script

--- CoopChrisGameJam1/Assets/Scripts/WallCollision.cs
+++ CoopChrisGameJam1/Assets/Scripts/WallCollision.cs
@@ -6,14 +6,21 @@ public class WallCollision : MonoBehaviour
 {
 	public const int BREAKING_VELOCITY = 2;
 
+	public int slowDown = 1;
+
     void OnCollisionEnter2D(Collision2D col)
     {
 
         if(col.relativeVelocity.magnitude > BREAKING_VELOCITY)
         {	Debug.Log("Over 2!");
 
+
+
+
         	if(CameraFollow.following == CameraFollow.truck) 
     		{
+    			Rigidbody2D rb = GameObject.Find("Truck").GetComponent<Movement>().rb;
+    			rb.velocity = slowDown*rb.velocity.normalized;
     			--PlayerStats.truckInventory;
     			if(PlayerStats.truckInventory < 0)
     			{
@@ -22,6 +29,9 @@ public class WallCollision : MonoBehaviour
     		}
     		else if(CameraFollow.following == CameraFollow.boat) 
     		{
+    			Rigidbody2D rb = GameObject.Find("Boat").GetComponent<Movement>().rb;
+    			rb.velocity = slowDown*rb.velocity.normalized;
+
     			--PlayerStats.boatInventory;
     			if(PlayerStats.boatInventory < 0)
     			{
@@ -30,6 +40,9 @@ public class WallCollision : MonoBehaviour
     		}
     		else if(CameraFollow.following == CameraFollow.tank) 
     		{
+    			Rigidbody2D rb = GameObject.Find("Tank").GetComponent<Movement>().rb;
+    			rb.velocity = slowDown*rb.velocity.normalized;
+
     			--PlayerStats.tankInventory;
     			if(PlayerStats.tankInventory < 0)
     			{
