eff3913d0bb4c4f436486959e865b82b15b21406
CooperRapp <61958386+CooperRapp@users.noreply.github.com>
2020-08-22 17:36:25 -0700

    changes

---
+++ CoopChrisGameJam1/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    void Game()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+}
