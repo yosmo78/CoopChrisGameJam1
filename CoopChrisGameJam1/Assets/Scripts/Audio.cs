f8b0418847c3fe7fa54bbefe9f95e67d8fff82b7
CooperRapp <61958386+CooperRapp@users.noreply.github.com>
2020-08-22 18:12:17 -0700

    changes

---
+++ CoopChrisGameJam1/Assets/Scripts/Audio.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+
+    public AudioSource gameSound;
+
+    void Start()
+    {
+
+        gameSound.Play();
+
+    }
+
+}
